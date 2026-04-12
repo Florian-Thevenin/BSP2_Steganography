5ed8a3f27f3a729101cb785d75c4a6df446fff77
Florian <Thevenin.florian@gmail.com>
2026-04-12 14:17:27 +0200

    Added Placeholders to hold more Encryption Algorithm

--- Crypto/Decrypt.py
+++ Crypto/Decrypt.py
@@ -6,3 +6,12 @@ def decrypt_data(data: bytes, key: bytes) -> bytes:
     fernet_key = base64.urlsafe_b64encode(key[:32])
     cipher = Fernet(fernet_key)
     return cipher.decrypt(data)
+
+
+# PLACEHOLDERS ( for future algorithms)
+def decrypt_second(data: bytes):
+    raise NotImplementedError("Other decryption not implemented yet")
+
+
+def decrypt_third(data: bytes):
+    raise NotImplementedError("Other decryption not implemented yet")
--- Crypto/Encrypt.py
+++ Crypto/Encrypt.py
@@ -6,3 +6,12 @@ def encrypt_data(data: bytes, key: bytes) -> bytes:
     fernet_key = base64.urlsafe_b64encode(key[:32])
     cipher = Fernet(fernet_key)
     return cipher.encrypt(data)
+
+
+# PLACEHOLDERS ( for future algorithms)
+def encrypt_second(data: bytes):
+    raise NotImplementedError("Other encryption not implemented yet")
+
+
+def encrypt_third(data: bytes):
+    raise NotImplementedError("Other encryption not implemented yet")
